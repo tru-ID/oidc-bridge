8bbc26f79199d3e32aed54c7b6dc07ac87154d6a
Luis Pedrosa <luis.pedrosa@tru.id>
2022-12-15 16:33:28 +0000

    add redis configuration override annotation
    The @EnableSentinel annotation overrides spring's default sentinel
    config setup.
    
    If this is not present, it will try to configure redis according to:
    https://docs.spring.io/spring-data/redis/docs/current/reference/html/#redis:sentinel

--- bridge/src/main/java/id/tru/oidc/sample/SampleApp.java
+++ bridge/src/main/java/id/tru/oidc/sample/SampleApp.java
@@ -3,6 +3,9 @@ package id.tru.oidc.sample;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import net._4uth.common.redis.config.EnableSentinel;
+
+@EnableSentinel
 @SpringBootApplication
 public class SampleApp {
     public static void main(String[] args) {
