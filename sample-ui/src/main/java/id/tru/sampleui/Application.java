48056d02f7958240aa75c67275100fd577a7b6e2
Luis Pedrosa <luis.pedrosa@tru.id>
2023-05-04 15:29:10 +0100

    add redis http session

--- sample-ui/src/main/java/id/tru/sampleui/Application.java
+++ sample-ui/src/main/java/id/tru/sampleui/Application.java
@@ -2,7 +2,9 @@ package id.tru.sampleui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
+@EnableRedisHttpSession
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
